e104765a65b5ef94076a4f3ca22302dd1a464073
21grahams <graham.kirsh@gmail.com>
2021-05-01 15:10:53 -0600

    fixed iFrame issue with body render issue

--- spaceJourney/src/components/APODPhotos.js
+++ spaceJourney/src/components/APODPhotos.js
@@ -6,15 +6,17 @@ const APODPhotos = (props) => {
       <span className="apodTitle">{props.title}</span>
       {props.link && props.link.indexOf("jpg") !== -1 ? (
         <img className="APODPhoto" src={props.link}></img>
-      ) : (
+      ) : props.link ? (
         <iframe
           className="APODPhoto"
           frameBorder="0"
+          width="600"
+          height="450"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
           src={props.link}
         ></iframe>
-      )}
+      ) : null}
       <p className="description">{props.description}</p>
     </div>
   );
